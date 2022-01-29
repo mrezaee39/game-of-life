7e4a53c64f2ffbdc99d74d59b75bd2a744b77561
Mohammad Rezaee <m.rezaee39@gmail.com>
2022-01-29 02:02:34 +0100

    first step till 9:33

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+void draw(void *u, int w, int h)
+{
+    char (*world)[w]=u;
+
+    for (int y=0;y<h;y++)
+    {
+        for (int)
+    }
+}
+void envolution(void *u, int w, int h)
+{
+
+}
+int main()
+{   
+    int w=30;
+    int h=30;
+    char world[w][h];
+
+    return 0;
+}
